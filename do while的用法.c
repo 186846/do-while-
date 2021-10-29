57a5802cc0f958a507beb3d96061abe75b989c94
鲁立 <1549717418@qq.com>
2021-10-30 00:55:13 +0800

    Initial commit

---
+++ do while的用法.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    int n=0;
+    scanf("%d",&x);
+    do{
+        x/=100;
+        n++;
+    }while(x>0);
+    printf("%d\n",n*=2);
+    return 0;
+}
